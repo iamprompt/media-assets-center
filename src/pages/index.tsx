6bc37bfdd447667ade1db5047a6e9b3503cf05d3
Supakarn Laorattanakul <wearedprompt@gmail.com>
2021-11-24 17:12:03 +0700

    Update SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import Layout from '../components/common/layout'
+import SEO from '../components/common/seo'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/search')
+  }, [])
+
   return (
-    <Layout>
-      <div className="pt-5"></div>
-    </Layout>
+    <SEO>
+      <Layout>
+        <div className="pt-5"></div>
+      </Layout>
+    </SEO>
   )
 }
 
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -7,6 +7,7 @@ import REGIONS from '../utils/constant/region'
 import Image from 'next/image'
 import { ResultItem, SearchResultResponse } from '../@types/api/atv-search'
 import { ResponseProps } from '../@types/api/common'
+import SEO from '../components/common/seo'
 
 export const getServerSideProps = async ({ query }: GetServerSidePropsContext) => {
   const q = query?.q && !Array.isArray(query.q) ? query.q : undefined
@@ -88,82 +89,84 @@ const SearchPage: NextPage<InferGetServerSidePropsType<typeof getServerSideProps
   }, [searchCountry, availableLocale])
 
   return (
-    <Layout>
-      <div className="pt-5 px-5 pb-10">
-        <form>
-          <div className="flex flex-col w-10/12 md:w-2/3 mx-auto space-y-5">
-            <h1 className="text-4xl font-bold text-center">Search</h1>
-            <div className="mt-1 relative rounded-xl shadow-sm">
-              <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">ค้นหา</span>
-              <input
-                type="text"
-                className="w-full rounded-xl border-gray-200 focus:border-gray-500 ring-0 p-2 pl-16 pr-16"
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-              />
-              <span className="absolute inset-y-0 right-0 flex items-center mr-12">
-                <select
-                  name="country"
-                  id="country"
-                  className="border-0 focus:ring-0 outline-transparent"
-                  onChange={(e) => setSearchCountry(e.target.value)}
-                  value={searchCountry}
-                >
-                  {Object.entries(REGIONS)
-                    .sort((a, b) => {
-                      return a[0] > b[0] ? 1 : -1
-                    })
-                    .map(([c, v]) => (
-                      <option key={v.name} value={c}>
-                        {c}
-                      </option>
-                    ))}
-                </select>
-              </span>
-              <span className="absolute inset-y-0 right-0 flex items-center w-5 h-full mr-3">
-                <button className="relative h-full w-full" type="submit" onClick={handleSearch}>
-                  <Image
-                    src="data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg' data-svg='search'%3E%3Ccircle fill='none' stroke='%23000' stroke-width='1.1' cx='9' cy='9' r='7'%3E%3C/circle%3E%3Cpath fill='none' stroke='%23000' stroke-width='1.1' d='M14,14 L18,18 L14,14 Z'%3E%3C/path%3E%3C/svg%3E"
-                    alt="Search"
-                    layout="fill"
-                  />
-                </button>
-              </span>
-            </div>
-          </div>
-        </form>
-        {d && (
-          <div className="mt-10">
-            <div className="text-center space-y-3 mb-5">
-              <h1 className="font-bold font-headline text-2xl">ผลการค้นหาสำหรับ</h1>
-              <h2 className="font-bold font-headline text-4xl">{currentSearchText}</h2>
-              <h2 className="font-headline text-1xl">{`${REGIONS[currentSearchCountry.toUpperCase()].name} - ${
-                REGIONS[currentSearchCountry.toUpperCase()].langs[currentSearchLocale]
-              }`}</h2>
-            </div>
-            <div className="space-y-10">
-              {Object.entries(d).map(([shelfId, shelf]) => {
-                return (
-                  <div key={shelfId}>
-                    <h2 className="font-bold text-4xl mb-5">{shelf.title}</h2>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
-                      {Object.entries(shelf.items).map(([cId, item]) => (
-                        <Card
-                          cId={cId}
-                          d={item}
-                          key={cId}
-                          option={{ country: currentSearchCountry, locale: currentSearchLocale }}
-                        />
+    <SEO title={currentSearchText ? `ผลการค้นหา ${currentSearchText}` : 'ค้นหา'}>
+      <Layout>
+        <div className="pt-5 px-5 pb-10">
+          <form>
+            <div className="flex flex-col w-10/12 md:w-2/3 mx-auto space-y-5">
+              <h1 className="text-4xl font-bold text-center">Search</h1>
+              <div className="mt-1 relative rounded-xl shadow-sm">
+                <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">ค้นหา</span>
+                <input
+                  type="text"
+                  className="w-full rounded-xl border-gray-200 focus:border-gray-500 ring-0 p-2 pl-16 pr-16"
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+                />
+                <span className="absolute inset-y-0 right-0 flex items-center mr-12">
+                  <select
+                    name="country"
+                    id="country"
+                    className="border-0 focus:ring-0 outline-transparent"
+                    onChange={(e) => setSearchCountry(e.target.value)}
+                    value={searchCountry}
+                  >
+                    {Object.entries(REGIONS)
+                      .sort((a, b) => {
+                        return a[0] > b[0] ? 1 : -1
+                      })
+                      .map(([c, v]) => (
+                        <option key={v.name} value={c}>
+                          {c}
+                        </option>
                       ))}
-                    </div>
-                  </div>
-                )
-              })}
+                  </select>
+                </span>
+                <span className="absolute inset-y-0 right-0 flex items-center w-5 h-full mr-3">
+                  <button className="relative h-full w-full" type="submit" onClick={handleSearch}>
+                    <Image
+                      src="data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg' data-svg='search'%3E%3Ccircle fill='none' stroke='%23000' stroke-width='1.1' cx='9' cy='9' r='7'%3E%3C/circle%3E%3Cpath fill='none' stroke='%23000' stroke-width='1.1' d='M14,14 L18,18 L14,14 Z'%3E%3C/path%3E%3C/svg%3E"
+                      alt="Search"
+                      layout="fill"
+                    />
+                  </button>
+                </span>
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-    </Layout>
+          </form>
+          {d && (
+            <div className="mt-10">
+              <div className="text-center space-y-3 mb-5">
+                <h1 className="font-bold font-headline text-2xl">ผลการค้นหาสำหรับ</h1>
+                <h2 className="font-bold font-headline text-4xl">{currentSearchText}</h2>
+                <h2 className="font-headline text-1xl">{`${REGIONS[currentSearchCountry.toUpperCase()].name} - ${
+                  REGIONS[currentSearchCountry.toUpperCase()].langs[currentSearchLocale]
+                }`}</h2>
+              </div>
+              <div className="space-y-10">
+                {Object.entries(d).map(([shelfId, shelf]) => {
+                  return (
+                    <div key={shelfId}>
+                      <h2 className="font-bold text-4xl mb-5">{shelf.title}</h2>
+                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+                        {Object.entries(shelf.items).map(([cId, item]) => (
+                          <Card
+                            cId={cId}
+                            d={item}
+                            key={cId}
+                            option={{ country: currentSearchCountry, locale: currentSearchLocale }}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          )}
+        </div>
+      </Layout>
+    </SEO>
   )
 }
 
