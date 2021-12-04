6aacc806817cad6a80b11358e61e6d9971efaa65
Supakarn Laorattanakul <wearedprompt@gmail.com>
2021-12-04 13:25:55 +0700

    Fix Existance of Trailers

--- src/pages/api/apple-tv/product.ts
+++ src/pages/api/apple-tv/product.ts
@@ -3,12 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { ProductResultResponse } from '../../../@types/platforms/apple-tv/api/atv-product'
 import { ResponseProps } from '../../../@types/api/common'
 import { ImagesFormat } from '../../../utils/api/helpers'
-import { APPLE_TV_API } from '../../../utils/platforms/apple-tv'
+import { APPLE_TV_API } from '../../../utils/platforms/apple-tv/helpers'
 
 const ProductAPI = async (req: NextApiRequest, res: NextApiResponse<ResponseProps<ProductResultResponse | string>>) => {
   const {
     query: { country, cId, locale },
   } = req
+
+  // console.log(country, cId, locale)
 
   if (Array.isArray(cId) || Array.isArray(country) || Array.isArray(locale))
     return res.status(400).json({ success: false, payload: 'Invalid query' })
@@ -24,6 +26,8 @@ const ProductAPI = async (req: NextApiRequest, res: NextApiResponse<ResponseProp
       locale: locale || 'th-TH',
     })
 
+    // console.log(content, related, roles, trailers)
+
     const payload: ProductResultResponse = {
       id: cId,
       result: {
@@ -38,35 +42,43 @@ const ProductAPI = async (req: NextApiRequest, res: NextApiResponse<ResponseProp
       },
     }
 
-    for (const role of roles) {
-      if (!payload.result.roles) payload.result.roles = []
-      payload.result.roles.push({
-        type: role.type,
-        roleTitle: role.roleTitle,
-        characterName: role.characterName,
-        images: ImagesFormat(role.images),
-        personName: role.personName,
-        personId: role.personId,
-        url: role.url,
-      })
-    }
-
-    for (const relatedItem of related.items) {
-      if (!payload.result.related) payload.result.related = {}
-      payload.result.related[relatedItem.id] = {
-        title: relatedItem.title,
-        images: ImagesFormat(relatedItem.images, ['coverArt', 'coverArt16X9']),
+    if (roles) {
+      for (const role of roles) {
+        if (!payload.result.roles) payload.result.roles = []
+        payload.result.roles.push({
+          type: role.type,
+          roleTitle: role.roleTitle,
+          characterName: role.characterName,
+          images: ImagesFormat(role.images),
+          personName: role.personName,
+          personId: role.personId,
+          url: role.url,
+        })
       }
     }
 
-    for (const trailer of trailers) {
-      if (!payload.result.trailers) payload.result.trailers = []
-      payload.result.trailers.push({
-        title: trailer.title,
-        hlsUrl: trailer.assets.hlsUrl,
-        duration: trailer.duration,
-      })
+    if (related) {
+      for (const relatedItem of related.items) {
+        if (!payload.result.related) payload.result.related = {}
+        payload.result.related[relatedItem.id] = {
+          title: relatedItem.title,
+          images: ImagesFormat(relatedItem.images, ['coverArt', 'coverArt16X9']),
+        }
+      }
     }
+
+    // if (trailers) {
+    //   for (const trailer of trailers) {
+    //     if (!payload.result.trailers) payload.result.trailers = []
+    //     payload.result.trailers.push({
+    //       title: trailer.title,
+    //       hlsUrl: trailer.assets.hlsUrl,
+    //       duration: trailer.duration,
+    //     })
+    //   }
+    // }
+
+    // console.log(payload)
 
     res.status(200).json({ success: true, payload })
   } catch (err) {
