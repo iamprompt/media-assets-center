d3341f254786c4941539ec4f3947b06044995d13
Supakarn Laorattanakul <wearedprompt@gmail.com>
2021-12-04 13:47:27 +0700

    Update User Agent

--- src/utils/platforms/disney-plus/helpers.ts
+++ src/utils/platforms/disney-plus/helpers.ts
@@ -43,6 +43,8 @@ export const GET_TOKEN = async () => {
     headers: {
       Authorization: BearerKey,
       'Content-Type': 'application/x-www-form-urlencoded',
+      'User-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
     },
   })
 
